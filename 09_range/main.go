2b3badd9ae6d9d1fcbe5134c0a5c975ed924a6bd
jordywestside <mm1jwest@gmail.com>
2020-12-14 21:03:05 -0600

    finished range

--- 09_range/main.go
+++ 09_range/main.go
@@ -3,5 +3,29 @@ package main
 import "fmt"
 
 func main() {
-  fmt.Println("Go world!")  
+	ids := []int{22,324,54,23,234}
+
+	// Loop through ids
+	for i, id := range ids {
+		fmt.Printf("%d - ID: %d\n", i, id)
+	}
+	// not using index
+	for _, id := range ids {
+		fmt.Printf("ID: %d\n", id)
+	}
+	// Add ids together
+	sum := 0
+	for _, id := range ids {
+		sum += id
+	}
+	fmt.Println("Sum", sum)
+	// Range with map
+	emails := map[string]string{"do":"do@mail.com", "doom":"doom@gmail.com"}
+	for k, v := range emails {
+		fmt. Printf("%s: %s\n", k, v)
+	}	
+	for k := range emails {
+		fmt.Println("Name: " + k)
+	}
 }
+
