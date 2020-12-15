6f28a324a5d215c1e9e5666d3ffea056da7b6da6
jordywestside <mm1jwest@gmail.com>
2020-12-14 20:51:46 -0600

    finished maps

--- 08_maps/main.go
+++ 08_maps/main.go
@@ -3,5 +3,23 @@ package main
 import "fmt"
 
 func main() {
-  fmt.Println("Go world!")  
+	// this is saying make a map, the keys a string, and the values a string
+	emails := make(map[string]string)
+	//this is the fast way
+	emails2 := map[string]string{"cricket":"cricket@email.com", "Zoe":"zoey@yahoo.com"}
+
+	//assign kv
+	emails["Bob"] = "bob@gmail.com"
+	emails["Squid"] = "squid@gmail.com"
+	emails["Mike"] = "mike@gmail.com"
+
+	fmt.Println(emails)
+	fmt.Println(emails2)
+	fmt.Println(len(emails))
+	fmt.Println(emails["Bob"])
+
+	// Delete from map
+	delete(emails, "Bob")
+	fmt.Println(emails)
+
 }
