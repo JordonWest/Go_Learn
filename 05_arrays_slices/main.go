8b0948d81c162519ca41ba439c8316ad8d09405a
jordywestside <mm1jwest@gmail.com>
2020-12-14 20:23:53 -0600

    arrays and slices done

--- 05_arrays_slices/main.go
+++ 05_arrays_slices/main.go
@@ -3,5 +3,23 @@ package main
 import "fmt"
 
 func main() {
-  fmt.Println("Go world!")  
+	// arrays
+//	var fruitArray [2]string
+	
+	//Assign values
+//	fruitArray[0] = "Apple"
+//	fruitArray[1] = "Dragonfruit"
+	
+	// shorter way of doing it:
+//	fruitArray2 := [2]string{"orange", "strawberry"}
+//	fmt.Println(fruitArray)
+//	fmt.Println(fruitArray2)
+//	fmt.Println(fruitArray[0])
+
+
+// SLICES
+	fruitSlice := []string{"apple", "tomato", "tacofruit"}
+	fmt.Println(fruitSlice)
+	fmt.Println(len(fruitSlice))
+	fmt.Println(fruitSlice[1:3])
 }
