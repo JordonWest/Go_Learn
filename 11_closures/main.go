956c116e0ee89f8e7c1ce51377457ad137598948
jordywestside <mm1jwest@gmail.com>
2020-12-14 21:15:48 -0600

    finished closures

--- 11_closures/main.go
+++ 11_closures/main.go
@@ -2,6 +2,19 @@ package main
 
 import "fmt"
 
+func adder() func(int) int {
+	sum:= 0
+	return func(x int) int {
+		sum += x
+		return sum
+	}
+}
+
 func main() {
-  fmt.Println("Go world!")  
+	sum := adder()
+	for i := 0; i < 10; i++ {
+	fmt.Println(sum(i))
+	}
+	
+
 }
