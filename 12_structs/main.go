a083cf80280642c6765cdb46a49d876dc38d64ff
jordywestside <mm1jwest@gmail.com>
2020-12-14 21:39:28 -0600

    finished structs

--- 12_structs/main.go
+++ 12_structs/main.go
@@ -1,6 +1,7 @@
 package main
 
-import "fmt"
+import ("fmt"
+		"strconv")
 
 // Define person struct
 type Person struct {
@@ -10,13 +11,37 @@ type Person struct {
 	gender string
 	age int
 }
-	
+// Greeting method (value receiver)
+func (p Person) greet() string{
+	return "Hello, my name is " + p.firstName + " " + p.lastName + " and I am " + strconv.Itoa(p.age) 
+}
+
+// getMarried (pointer receiver)
+func (p *Person) getMarried(spouseLastName string) {
+	if p.gender == "m" {
+		return
+	} else {
+		p.lastName = spouseLastName
+	}
+}
+// hasBirthday method (pointer receiver)
+func (p *Person) hasBirthday() {
+	p.age++
+}
+
 
 func main() {
 	// Init person using struct
-	person1 := Person{firstName:"Squid", lastName:"Boy", city: "House", gender: "f", age: 4}
+	person1 := Person{firstName:"Squid", lastName:"Boy", city: "House", gender: "m", age: 4}
 	//alternative (it accepts them as positional arguments)
-	person2 := Person{"Squid", "Boy", "House", "f", 4}
+	person2 := Person{"Squid", "Boy", "House", "m", 4}
+	person3 := Person{"Tibby", "ladyington", "House", "f", 4}
+	person1.age++
 	fmt.Println(person1)
 	fmt.Println(person2)
+	fmt.Println(person3)
+	person1.hasBirthday()
+	person3.getMarried("Williams")
+	fmt.Println(person3.greet())
+
 }
