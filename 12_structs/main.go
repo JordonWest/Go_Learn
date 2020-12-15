4120c947a363f829b2bd3ceaca58692852295790
jordywestside <mm1jwest@gmail.com>
2020-12-14 21:25:39 -0600

    struct built and working

--- 12_structs/main.go
+++ 12_structs/main.go
@@ -2,6 +2,21 @@ package main
 
 import "fmt"
 
+// Define person struct
+type Person struct {
+	firstName string
+	lastName string
+	city string
+	gender string
+	age int
+}
+	
+
 func main() {
-  fmt.Println("Go world!")  
+	// Init person using struct
+	person1 := Person{firstName:"Squid", lastName:"Boy", city: "House", gender: "f", age: 4}
+	//alternative (it accepts them as positional arguments)
+	person2 := Person{"Squid", "Boy", "House", "f", 4}
+	fmt.Println(person1)
+	fmt.Println(person2)
 }
