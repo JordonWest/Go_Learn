ad0b16b93758994e62241c7c09d2c041333b8300
jordywestside <mm1jwest@gmail.com>
2020-12-14 21:52:59 -0600

    web working and serving!!!

--- 14_web/main.go
+++ 14_web/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import ("fmt"
+		"net/http")
+
+func index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Hello World</h1>")
+}
+func about(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>About</h1>")
+}
 
 func main() {
-  fmt.Println("Go world!")  
+	http.HandleFunc("/", index)
+	http.HandleFunc("/about", about)
+	fmt.Println("server running...")
+	http.ListenAndServe(":3000", nil)
 }
