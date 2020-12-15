c62635e78cef1ae5918a10d7e9c4821629ad05e3
jordywestside <mm1jwest@gmail.com>
2020-12-14 20:31:49 -0600

    finished conditionals

--- 06_conditionals/main.go
+++ 06_conditionals/main.go
@@ -3,5 +3,31 @@ package main
 import "fmt"
 
 func main() {
-  fmt.Println("Go world!")  
+	x := 56
+	y := 10
+
+	if x <= y {
+		fmt.Printf("%d is less than or equal to %d\n", x, y)
+	} else{
+		fmt.Printf("%d is less than %d\n", y, x)
+	}
+
+	color := "green"
+
+	if color == "red" {
+		fmt.Println("color is red")
+	} else if color == "blue" {
+		fmt.Println("color is blue")
+	} else {
+		fmt.Println("color is not red or blue")
+	}
+	// Switch
+	switch color {
+	case "red":
+		fmt.Println("color is red")
+	case "blue":
+		fmt.Println("color is blue")
+	default:
+		fmt.Println("color is not blue OR red")
+	}
 }
